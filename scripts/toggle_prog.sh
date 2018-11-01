a01c044641b1d060456949f07bb3e864af4c34d6
littlecoce <gitmessage@mailbox.org>
2018-11-01 22:16:39 +0100

    Added toggle menu and script to kill compton and xautolock

---
+++ scripts/toggle_prog.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ $1 == compton ]]; then
+	PROG=compton
+elif [[ $1 == xautolock ]]; then
+	PROG=xautolock
+else
+	exit 1;
+fi
+
+pidof $PROG 2>&1 > /dev/null
+
+if [[ $? -eq 0 ]]; then
+	killall $PROG
+	if [[ $? -eq 0 ]]; then
+		notify-send $PROG "Deactivated"
+	else
+		notify-send $PROG "A problem occured (deacvtivate)"
+	fi
+else
+	if [[ $PROG == compton ]]; then
+		compton -b --config ~/.config/.compton.conf
+	elif [[ $PROG == xautolock ]]; then 
+		~/.config/scripts/xautolock.sh &
+	fi
+	if [[ $? -eq 0 ]]; then
+		notify-send $PROG "Activated"
+	else
+		notify-send $PROG "A problem occured (activate)"
+	fi
+fi
