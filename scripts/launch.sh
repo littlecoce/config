991f3d8d01d11d498e2576f438d60ebe2fb01da8
littlecoce <gitmessage@mailbox.org>
2019-02-12 17:21:19 +0100

    Resolved small mistake in polybar launch script

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload top &
 done
 
 echo "Bars launched..."
