08671cb9a4e8a29028fae54185b8278a1ad7baff
littlecoce <gitmessage@mailbox.org>
2019-05-04 17:23:18 +0200

    Restrucured to make it easier to set thresholds for warning and hibernating limits

--- scripts/check_bat.sh
+++ scripts/check_bat.sh
@@ -2,6 +2,20 @@
 
 # Remember to create a cron job to execute regular
 
+# WCAP is the warning threshold, 
+# HCAP threshold to hibernate
+WCAP=20
+HCAP=5
+
+# Delay to wait until NB hibernates under WCAP value
+DELAY=30
+
+# Messages to display
+MSG="CAP is lower than ${WCAP}"
+MSG_CRIT="CAP is at ${HCAP}
+Hibernating in ${DELAY} Seconds
+Stop with killall check_bat.sh"
+
 # Sets dbus address variable to display dunst messages correctly 
 # when scheduled via cron. Check dbus_session.sh script!
 if [ -z $HOME/.dbus/.dbus_addr ] ; then
@@ -13,19 +27,10 @@ fi
 CAP=$(</sys/class/power_supply/BAT0/capacity)
 STAT=$(</sys/class/power_supply/BAT0/status)
 
-# Delay to wait until NB hibernates
-DELAY=30
-
-# Messages to display
-MSG="CAP is lower than 15%"
-MSG_CRIT="CAP is at 5%
-Hibernating in ${DELAY} Seconds
-Stop with killall check_bat.sh"
-
 if [ "${STAT,,}x" == "dischargingx" ] ; then
-	if [[ ${CAP} -lt 20 && ${CAP} -gt 5 ]] ; then
+	if [[ ${CAP} -lt ${WCAP} && ${CAP} -gt 5 ]] ; then
 		notify-send -u critical Battery "${MSG}"
-	elif [ ${CAP} -le 5 ] ; then
+	elif [ ${CAP} -le ${HCAP} ] ; then
 		notify-send -u critical Battery "${MSG_CRIT}"
 		sleep ${DELAY}
 		STAT=$(</sys/class/power_supply/BAT0/status)
