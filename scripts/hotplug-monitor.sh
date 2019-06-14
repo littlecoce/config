03fc127568253c96601801843cd19e8d5b8299e6
littlecoce <gitmessage@mailbox.org>
2019-06-14 08:17:19 +0200

    cleared typo in autologin README

--- scripts/hotplug-monitor.sh
+++ scripts/hotplug-monitor.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-DP1=$(</sys/class/drm/card0-DP-1/status)
-VGA=$(</sys/class/drm/card0-VGA-1/status)
-
-if [ "connected" == "$DP1" ]; then
-	/usr/bin/xrandr --output DisplayPort-0 --left-of LVDS --auto
-	if [ "connected" == "$VGA" ]; then
-		/usr/bin/xrandr --output VGA-0 --left-of DisplayPort-0 --auto
-	else
-		/usr/bin/xrandr --output VGA-0 --off
-	fi
-	/usr/bin/notify-send --urgency=low "Monitor Hotplug" "Monitor(s) connected"
-elif [ "connected" == "$VGA" ]; then
-	/usr/bin/xrandr --output VGA-0 --left-of LVDS --auto
-	/usr/bin/xrandr --output DisplayPort-0 --off
-	/usr/bin/notify-send --urgency=low "Monitor Hotplug" "VGA-0 connected"
-else
-	/usr/bin/xrandr --output VGA-0 --off
-	/usr/bin/xrandr --output DisplayPort-0 --off
-	/usr/bin/notify-send --urgency=low "Monitor Hotplug" "Second Monitor disconnected"
-fi
+# DVI=$(</sys/class/drm/card0-DP-1/status)
+# VGA=$(</sys/class/drm/card0-VGA-1/status)
+# 
+# if [ "connected" == "$DVI" ]; then
+# 	/usr/bin/xrandr --output DisplayPort-0 --left-of LVDS --auto
+# 	if [ "connected" == "$VGA" ]; then
+# 		/usr/bin/xrandr --output VGA-0 --left-of DisplayPort-0 --auto
+# 	else
+# 		/usr/bin/xrandr --output VGA-0 --off
+# 	fi
+# 	/usr/bin/notify-send --urgency=low "Monitor Hotplug" "Monitor(s) connected"
+# elif [ "connected" == "$VGA" ]; then
+# 	/usr/bin/xrandr --output VGA-0 --left-of LVDS --auto
+# 	/usr/bin/xrandr --output DisplayPort-0 --off
+# 	/usr/bin/notify-send --urgency=low "Monitor Hotplug" "VGA-0 connected"
+# else
+# 	/usr/bin/xrandr --output VGA-0 --off
+# 	/usr/bin/xrandr --output DisplayPort-0 --off
+# 	/usr/bin/notify-send --urgency=low "Monitor Hotplug" "Second Monitor disconnected"
+# fi
+xrandr --output VGA-0 --right-of DVI-D-0 --auto
 /usr/bin/sh ~/.config/scripts/launch.sh 1>&2 >/dev/null
 /usr/bin/feh --bg-fill ~/.config/default/current.jpg
 /usr/bin/betterlockscreen -u ~/.config/default/lock.jpg
