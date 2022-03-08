bd06b9f053751816902bd56f813ca9a2b093acbe
littlecoce <gitmessage@mailbox.org>
2022-03-08 11:22:03 +0100

    Removed some files

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -4,7 +4,7 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar -u "$UID"  2>&1 >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload top &
